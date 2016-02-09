fe0451e6a9eec2c9464c420d6bcf08f7872ea2b9
pnmougel <pnmougel@gmail.com>
2016-02-09 16:29:48 +0100

    Added an iterator over the json file

---
+++ src/main/scala/shared/JsonIterator.scala
@@ -0,0 +1,44 @@
+package shared
+
+import java.io.File
+
+import scala.io.Source
+
+/**
+ * Created by nico on 09/02/16.
+ */
+class JsonIterator(file: File, maxJson: Int = -1, showProgress: Boolean = true) {
+  def forEach(f: String => Unit): Unit = {
+    val startTime = System.currentTimeMillis()
+    var i = 1
+    var done = 0L
+    val fileSize = file.length()
+
+    for (line <- Source.fromFile(file).getLines()) {
+      val length = line.size
+      done += length
+
+      if (i % 10000 == 0 && showProgress) {
+        val curTime = System.currentTimeMillis()
+        val remainingBytes = fileSize - done
+        val secElapsed = (curTime - startTime).toDouble / 1000
+        val bytesPerSec = done.toDouble / secElapsed
+        val donePercentage = 100 * done.toDouble / fileSize
+        val minutesRemaining = remainingBytes / bytesPerSec
+
+        println(f"Done ${donePercentage}%.0f" + "%" + f" \tat ${bytesPerSec / (1024 * 1024)}%.2f Mb/sec. \tApprox ${minutesRemaining}%.0f sec remaining")
+      }
+      if(maxJson != -1 && maxJson < i - 1) {
+        return
+      }
+      i += 1
+
+
+      if (length != 1 && line != "[" && line != "]") {
+        val lastChar = line.charAt(length - 1)
+        val content = if (lastChar == ',') line.substring(0, length - 1) else line
+        f(content)
+      }
+    }
+  }
+}
